f07bef3b8159f58b982cd48a46d0f46f7d17e925
Shrouk Abozeid <shroookabozaid@gmail.com>
2018-07-28 02:34:07 +0200

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,20 @@
 class Article < ApplicationRecord
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+	def tag_list
+		self.tags.collect do |tag|
+			tag.name
+		end.join(", ")
+	end
+
+	def tag_list=(tags_string)
+		tags=tags_string.split(",")
+		tags = tags.collect{|t| t.strip.downcase}
+		tags = tags.uniq
+		tags.each do |t|
+			tmp= Tag.find_or_create_by(name: t)
+			self.tags.push(tmp)
+		end
+	end
 end
